b83c9e82eaef7d7995ff95800d08eb6034efe543
Ankush511 <511ankush007@gmail.com>
2023-03-30 23:20:11 +0530

    categorywise-news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,6 +10,7 @@ export class NewsItem extends Component {
         <img src={!imageUrl? "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?size=626&ext=jpg": imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
+          {/* <p className="card-date{{">{publishedAt}</p> */}
           <p className="card-text">{description}...</p>
           <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
         </div>
